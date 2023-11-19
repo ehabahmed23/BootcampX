149a69c7b53c0b414e75ae33f877e73daff4ec71
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-11-19 00:14:05 -0500

    parameterized queries teachers and students

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const query = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+const values = [`%${process.argv[2]}%`]
+pool.query(query, values)
 .then(res => {
-  res.rows.forEach(row => {
+  res.rows.forEach((row) => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
 });
